aa4b7105e7db61ccd365ddede9c0557c3b59cef5
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-08-16 23:41:50 +0900

    GridViewにはshrinkWrapを設定する

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_app/sample_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.orange),
-        home: Scaffold(
-            appBar: AppBar(title: const Text("mmmmm")),
-            body: Column(children: [
-              Theme(
-                  data: ThemeData(primaryColor: Colors.red),
-                  child: const Sample(title: "Widget A")),
-              Column(
-                children: [
-                  const Sample(title: "Widget B"),
-                  Theme(
-                    data: ThemeData(primaryColor: Colors.blue),
-                    child: const Sample(title: "Widget C"),
-                  )
-                ],
-              )
-            ])));
+      // theme: ThemeData(primaryColor: Colors.orange),
+      theme: ThemeData.from(
+          colorScheme: const ColorScheme.light(
+        primary: Colors.pink,
+        onPrimary: Colors.white,
+        secondary: Colors.green,
+      )),
+      home: const SamplePage(),
+    );
+    // home: Scaffold(
+    //     appBar: AppBar(title: const Text("mmmmm")),
+    //     body: Column(children: [
+    //       Theme(
+    //           data: ThemeData(primaryColor: Colors.red),
+    //           child: const Sample(title: "Widget A")),
+    //       Column(
+    //         children: [
+    //           const Sample(title: "Widget B"),
+    //           Theme(
+    //             data: ThemeData(primaryColor: Colors.blue),
+    //             child: const Sample(title: "Widget C"),
+    //           )
+    //         ],
+    //       )
+    //     ])));
   }
 }
 
-class Sample extends StatelessWidget {
-  const Sample({Key? key, required this.title}) : super(key: key);
+// class Sample extends StatelessWidget {
+//   const Sample({Key? key, required this.title}) : super(key: key);
 
-  final String title;
+//   final String title;
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: 200,
-        height: 100,
-        child: ColoredBox(
-          color: Theme.of(context).primaryColor,
-          child: Text(title),
-        ));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//         width: 200,
+//         height: 100,
+//         child: ColoredBox(
+//           color: Theme.of(context).primaryColor,
+//           child: Text(title),
+//         ));
+//   }
+// }
--- lib/sample_page.dart
+++ lib/sample_page.dart
@@ -7,11 +7,24 @@ class SamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("title")),
-        body: Container(
-            color: Colors.amber,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [Text("hoge"), Text("fuga"), Text("piyo")],
-            )));
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("hoge"),
+            const Text("fuga"),
+            const Text("piyo"),
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              children: [
+                for (var i = 0; i < 9; i++)
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ColoredBox(color: Colors.blue),
+                  )
+              ],
+            )
+          ],
+        ));
   }
 }
